a4ec9692996da0a7421977b7e38fec85a03ec0a9
Luca Galasso <galasso.luca@outlook.com>
2018-07-15 15:56:16 +0200

    multiple lines with different delimiter

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,15 +1,19 @@
 module.exports = class Calculator {
-    static add(string) {
+    static add(string, delimiter = ',') {
         if ('string' !== typeof string)
             throw new Error('not a string')
 
         const lines = string.split('\n')
         if (lines[0].startsWith('//')) {
-            return 3
+            if(!lines[0].charAt(2))
+                throw new Error('undefined delimiter')
+
+            delimiter = lines[0].charAt(2)
+            lines.shift()
         }
 
         const globalSum = lines.reduce((globalAcc, line) => {
-            const lineSum = line.split(',')
+            const lineSum = line.split(delimiter)
                 .map(numberAsString => Number(numberAsString))
                 .reduce((lineAcc, number) => lineAcc + number, 0)
             return globalAcc + lineSum
