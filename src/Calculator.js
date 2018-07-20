e82ec9f5d57b5e71f5da8fee9e1499ad16b9a27c
Luca Galasso <galasso.luca@outlook.com>
2018-07-20 09:34:21 +0200

    changed the static method into a function

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,39 +1,37 @@
-module.exports = class Calculator {
-    static add(string) {
-        if ('string' !== typeof string)
-            throw new Error('not a string')
+module.exports = calculator = (string) => {
+    if ('string' !== typeof string)
+        throw new Error('not a string')
 
-        const lines = string.split('\n')
-        let delimiter = ','
+    const lines = string.split('\n')
+    let delimiter = ','
 
-        // check if exist an alternative delimiter
-        if (lines[0].startsWith('//')) {
-            if (!lines[0].charAt(2))
-                throw new Error('undefined delimiter')
+    // check if exist an alternative delimiter
+    if (lines[0].startsWith('//')) {
+        if (!lines[0].charAt(2))
+            throw new Error('undefined delimiter')
 
-            delimiter = lines[0].charAt(2)
-            lines.shift()
-        }
-
-        const negativeNumbers = []
-        const acceptedNumbers = []
-
-        // find and store all numbers
-        lines.forEach((line) => {
-            line.split(delimiter)
-                .forEach(numberAsString => {
-                    const number = Number(numberAsString)
-                    if (number < 0)
-                        negativeNumbers.push(number)
-                    else if (number <= 1000)
-                        acceptedNumbers.push(number)
-                })
-        })
-
-        if (negativeNumbers.length > 0)
-            throw Error(`negatives not allowed: ${negativeNumbers.join(' ')}`)
-
-        const globalSum = acceptedNumbers.reduce((acc, number) => acc + number, 0)
-        return globalSum
+        delimiter = lines[0].charAt(2)
+        lines.shift()
     }
+
+    const negativeNumbers = []
+    const acceptedNumbers = []
+
+    // find and store all numbers
+    lines.forEach((line) => {
+        line.split(delimiter)
+            .forEach(numberAsString => {
+                const number = Number(numberAsString)
+                if (number < 0)
+                    negativeNumbers.push(number)
+                else if (number <= 1000)
+                    acceptedNumbers.push(number)
+            })
+    })
+
+    if (negativeNumbers.length > 0)
+        throw Error(`negatives not allowed: ${negativeNumbers.join(' ')}`)
+
+    const globalSum = acceptedNumbers.reduce((acc, number) => acc + number, 0)
+    return globalSum
 }
--- test/CalculatorTest.js
+++ test/CalculatorTest.js
@@ -1,11 +1,11 @@
 const { expect } = require('chai')
-const Calculator = require('../src/Calculator')
+const calculator = require('../src/Calculator')
 
 
 describe('Calculator tests', () => {
     it('Empty string should return 0', () => {
         const emptyString = ""
-        const result = Calculator.add(emptyString)
+        const result = calculator(emptyString)
 
         expect(result).to.equal(0)
     })
@@ -13,26 +13,26 @@ describe('Calculator tests', () => {
     it('A number should throw an error', () => {
         const number = 12
 
-        expect(() => { Calculator.add(number) }).to.throw(/not a string/)
+        expect(() => { calculator(number) }).to.throw(/not a string/)
     })
 
     it('A string number should return the given number', () => {
         const stringNumber = "12"
-        const result = Calculator.add(stringNumber)
+        const result = calculator(stringNumber)
 
         expect(result).to.equal(12)
     })
 
     it('Should sum 3 numbers separated with comma', () => {
         const threeNumbersSeparatedWithComma = "1,2,3"
-        const result = Calculator.add(threeNumbersSeparatedWithComma)
+        const result = calculator(threeNumbersSeparatedWithComma)
 
         expect(result).to.equal(6)
     })
 
     it('Should sum two lines of numbers separated with \\n', () => {
         const twoLinesOfNumbersSeparatedWithComma = "1\n2,3"
-        const result = Calculator.add(twoLinesOfNumbersSeparatedWithComma)
+        const result = calculator(twoLinesOfNumbersSeparatedWithComma)
 
         expect(result).to.equal(6)
     })
@@ -40,12 +40,12 @@ describe('Calculator tests', () => {
     it('Should generate error if different delimiter is expected but it is not present', () => {
         const stringWithUndefinedDelimiter = "//\n1;2"
 
-        expect(() => { Calculator.add(stringWithUndefinedDelimiter) }).throw(/undefined delimiter/)
+        expect(() => { calculator(stringWithUndefinedDelimiter) }).throw(/undefined delimiter/)
     })
 
     it('Should accept a different delimiter stored at the first line if it starts with //', () => {
         const twoLineWithDifferentDelimiter = "//;\n1;2\n3"
-        const result = Calculator.add(twoLineWithDifferentDelimiter)
+        const result = calculator(twoLineWithDifferentDelimiter)
 
         expect(result).to.equal(6)
     })
@@ -53,12 +53,12 @@ describe('Calculator tests', () => {
     it('Should throw an error followed by all negative numbers if negative numbers are given', () => {
         const unexpectedNegativeNumbers = "1,-2,3,-5"
 
-        expect(() => { Calculator.add(unexpectedNegativeNumbers) }).throw(/negatives not allowed: -2 -5/)
+        expect(() => { calculator(unexpectedNegativeNumbers) }).throw(/negatives not allowed: -2 -5/)
     })
 
     it('Numbers bigger than 1000 should be ignored', () => {
         const lineWithHugeNumber = '2,1001'
-        const result = Calculator.add(lineWithHugeNumber)
+        const result = calculator(lineWithHugeNumber)
 
         expect(result).to.equal(2)
     })
